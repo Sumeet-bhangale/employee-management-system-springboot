8f0d2cac9a0ea84f818f381fb6c962db5dfc8c24
Sumeet Bhangale <sumeetbhangale00@gmail.com>
2024-06-09 10:59:15 +0530

    Added Department Module

--- employee-management-system-springboot/src/main/java/com/EmpManagement/Controller/DepartmentController.java
+++ employee-management-system-springboot/src/main/java/com/EmpManagement/Controller/DepartmentController.java
@@ -1,14 +1,19 @@
 package com.EmpManagement.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.EmpManagement.Entity.Department;
+
 import com.EmpManagement.Service.DepartmentService;
 
 @RestController
@@ -28,5 +33,25 @@ public class DepartmentController {
 	public List<Department> getAllDepartment() {
 		return departmentService.getAllDepartments();
 	}
-
+	
+	// Get Department By Id
+	@GetMapping("/departments/{id}")
+	public Optional<Department> getDepartment(@PathVariable int id) {
+		return departmentService.getDepartment(id);
+		
+	}
+	
+	// Update Department By id
+	@PutMapping("/departments/{id}")
+	public Department updateDepartment(@PathVariable("id") int id, @RequestBody Department dept) {
+		return departmentService.updateDepartment(id, dept);
+		
+	}
+	
+	// Delete Department by Id
+	@DeleteMapping("/departments/{id}")
+	public String deleteDepartmentById(@PathVariable("id") int id) {
+		return departmentService.deleteDepartmentById(id);
+	}
+	
 }
--- employee-management-system-springboot/src/main/java/com/EmpManagement/Service/DepartmentService.java
+++ employee-management-system-springboot/src/main/java/com/EmpManagement/Service/DepartmentService.java
@@ -1,6 +1,8 @@
 package com.EmpManagement.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,16 +16,49 @@ public class DepartmentService {
 	@Autowired
 	private DepartmentRepository departmentRepository;
 
-	
 	// Add Department
 	public void addDepartment(Department d) {
 		departmentRepository.save(d);
 	}
 
-	
 	// Get All Departments
 	public List<Department> getAllDepartments() {
 		List<Department> depts = (List<Department>) departmentRepository.findAll();
 		return depts;
 	}
+
+	// Get Department By Id
+	public Optional<Department> getDepartment(int id) {
+		return departmentRepository.findById(id);
+
+	}
+
+	// Update Department Details
+	public Department updateDepartment(int id, Department department) {
+		Optional<Department> department1 = departmentRepository.findById(id);
+
+		if (department1.isPresent()) {
+			Department originalDepartment = department1.get();
+
+			if (Objects.nonNull(department.getDepartment_name())
+					&& !"".equalsIgnoreCase(department.getDepartment_name())) {
+				originalDepartment.setDepartment_name(department.getDepartment_name());
+			}
+			if (Objects.nonNull(department.getShort_name()) && !"".equals(department.getShort_name())) {
+				originalDepartment.setShort_name(department.getShort_name());
+			}
+			return departmentRepository.save(originalDepartment);
+		}
+		return null;
+	}
+
+	// Delete Department
+	public String deleteDepartmentById(int id) {
+		if (departmentRepository.findById(id).isPresent()) {
+			departmentRepository.deleteById(id);
+			return "Department deleted successfully";
+		}
+		return "No such Department in the database";
+	}
+
 }
